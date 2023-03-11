5a44a6d367194eade77e6073e85427fd8dae76b9
Ardit <98714739+ArditQerimi@users.noreply.github.com>
2023-03-12 00:42:00 +0100

    user-home-2

--- src/views/User/HomeUIView.vue
+++ src/views/User/HomeUIView.vue
@@ -38,14 +38,51 @@ export default {
         'Fourth',
         'Fifth',
       ],
+      sidebar: true,
+      searchTerm: '',
+      selectedCategory: '',
+      selectedColor: '',
+      selectedSize: '',
+      products: [
+        { name: 'Product A', category: 'Category 1', color: 'Red', size: 'Small', image: 'https://source.unsplash.com/500x500/?product,red' },
+        { name: 'Product B', category: 'Category 1', color: 'Blue', size: 'Medium', image: 'https://source.unsplash.com/500x500/?product,blue' },
+        { name: 'Product C', category: 'Category 2', color: 'Green', size: 'Large', image: 'https://source.unsplash.com/500x500/?product,green' },
+        { name: 'Product D', category: 'Category 3', color: 'Yellow', size: 'Small', image: 'https://source.unsplash.com/500x500/?product,yellow' },
+        { name: 'Product E', category: 'Category 3', color: 'Pink', size: 'Medium', image: 'https://source.unsplash.com/500x500/?product,pink' },
+      ],
+      categories: ['Category 1', 'Category 2', 'Category 3'],
+      colors2: ['Red', 'Blue', 'Green', 'Yellow'],
+      sizes: ['Small', 'Medium', 'Large'],
+      icons: [
+        'mdi-facebook',
+        'mdi-twitter',
+        'mdi-linkedin',
+        'mdi-instagram',
+      ],
     }
   },
-  computed: {
 
+  computed: {
     ...mapState({
       user: (state) => state.auth.user,
       products:(state)=>state.auth.products
     }),
+    filteredProducts() {
+      let filtered = this.products;
+      if (this.searchTerm) {
+        filtered = filtered.filter(product => product.name.toLowerCase().includes(this.searchTerm.toLowerCase()));
+      }
+      if (this.selectedCategory) {
+        filtered = filtered.filter(product => product.category === this.selectedCategory);
+      }
+      if (this.selectedColor) {
+        filtered = filtered.filter(product => product.color === this.selectedColor);
+      }
+      if (this.selectedSize) {
+        filtered = filtered.filter(product => product.size === this.selectedSize);
+      }
+      return filtered;
+    },
   },
   methods: {
     push() {
@@ -77,11 +114,10 @@ export default {
 </script>
 <template>
   <v-app>
-    <v-app-bar app >
-      <div style="margin-inline-start: 10px; margin-right: 10px">
+    <v-app-bar app style="position: relative">
+      <div style="margin-inline: 10px">
         <v-img src="https://wtmarketing.com/wp-content/uploads/2019/04/1280px-Shopify_logo_2018.svg.png" alt="Logo" style="width:100px; height: 50px"></v-img>
       </div>
-<!--      <v-toolbar-title style="width: 125px"><v-img src="https://wtmarketing.com/wp-content/uploads/2019/04/1280px-Shopify_logo_2018.svg.png" alt="Logo" style="width:125px; height: 50px"></v-img></v-toolbar-title>-->
       <v-spacer></v-spacer>
 
       <div style="display: flex; width:400px; align-items:center; gap:5px">
@@ -102,7 +138,7 @@ export default {
       </div>
 
 
-      <v-menu v-if="this.user">
+      <v-menu v-if="this.user"  >
         <template v-slot:activator="{ props }">
           <v-btn icon v-bind="props">
             <v-avatar>
@@ -140,12 +176,13 @@ export default {
       </div>
 
     </v-app-bar>
-    <v-main>
+    <v-main style="  --v-layout-top: 0px;
+  padding-top: var(--v-layout-top);">
       <v-carousel
           cycle
           height="400"
-          hide-delimiter-background
           show-arrows="hover"
+hide-delimiter-background
       >
         <v-carousel-item
             v-for="(slide, i) in slides"
@@ -163,10 +200,83 @@ export default {
           </v-sheet>
         </v-carousel-item>
       </v-carousel>
+
+      <v-container fluid>
+        <v-row>
+          <v-col cols="12" sm="6" md="3">
+            <v-sidebar v-model="sidebar" app>
+              <v-list>
+                <v-list-item>
+                  <v-text-field v-model="searchTerm" label="Search by name" />
+                </v-list-item>
+                <v-list-item>
+                  <v-select v-model="selectedCategory" :items="categories" label="Category" />
+                </v-list-item>
+                <v-list-item>
+                  <v-select v-model="selectedColor" :items="colors" label="Color" />
+                </v-list-item>
+                <v-list-item>
+                  <v-select v-model="selectedSize" :items="sizes" label="Size" />
+                </v-list-item>
+              </v-list>
+            </v-sidebar>
+          </v-col>
+          <v-col cols="12" sm="6" md="9">
+            <v-row>
+              <v-col v-for="(product, index) in filteredProducts" :key="index" cols="12" sm="12" md="6" lg="4">
+                <v-card>
+                  <v-img :src="product.image" height="200"></v-img>
+                  <v-card-title>{{ product.name }}</v-card-title>
+                  <v-card-text>
+                    <p>Category: {{ product.category }}</p>
+                    <p>Color: {{ product.color }}</p>
+                    <p>Size: {{ product.size }}</p>
+                  </v-card-text>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
-    <v-footer app>
-      <!-- Footer content -->
+        <v-footer
+            class="bg-indigo-lighten-1 text-center d-flex flex-column"
+            style="background-color: #1E88E5 !important;"
+        >
+          <div>
+            <v-btn
+                v-for="icon in icons"
+                :key="icon"
+                class="mx-4"
+                :icon="icon"
+                variant="text"
+            ></v-btn>
+          </div>
+
+          <div class="pt-0">
+            We are a leading e-commerce platform offering a wide selection of products including clothing, electronics, home goods, and more. Our mission is to provide our customers with the best shopping experience possible, from the moment they browse our products to the moment they receive their order. We are committed to offering high-quality products at affordable prices, and we are constantly updating our inventory to stay up-to-date with the latest trends and technology.
+          </div>
+
+          <v-divider></v-divider>
+
+          <div>
+            {{ new Date().getFullYear() }} — <strong>My E-Commerce App</strong>
+          </div>
+
     </v-footer>
   </v-app>
 
 </template>
+<style>
+.v-carousel .v-window-dots .v-window-dot {
+  background-color: white !important;
+}
+
+.v-carousel__controls .v-btn .v-btn__content .v-icon {
+  color: white;
+}
+.v-carousel__controls .v-btn .v-btn__content  .v-btn--active .v-icon {
+  background:white;
+
+}
+</style>
